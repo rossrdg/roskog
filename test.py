73291530c949506229438f9bd92a1b5c23809725
rossrdg <162810597+rossrdg@users.noreply.github.com>
2024-03-10 12:31:22 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import random
-ss = [1,2,3,4,5,6q,w,e,r,t,!,S,D]
-pass = random(ss)
-print("testcase")
-print("another test req")
-print(pass)
+setnum=[1,2,3,4,5,6]
+password=[]
+for i in range(12):
+    aa = random.choice(setnum)
+    password.append(aa)
+print(f"The password is : {password}")
