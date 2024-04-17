c9ac14e00ea2db61df6e04fc78cd2b419deafefa
rossrdg <162810597+rossrdg@users.noreply.github.com>
2024-04-17 10:32:41 +0300

    Update toolkit.sh

--- toolkit.sh
+++ toolkit.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
-PS3="Which script would you like to run?: "
-select script in "Cruft Remover" "Folder Organiser"; do
-	case "$script" in
-		"Cruft Remover") echo "Loading script.."
-				sleep 2
-				./cruft_remover.sh	
-				;;
-		"Folder Organiser") echo "Loading script.."
-				sleep 2
-				./folder_organiser.sh
-				;;
-		*) echo "Invalid Option";;
-	esac
-break
-done		
 
+# Author: Rosen Gurgov
+
+# Created Date: 04/15/2024
+
+# Last Modified Date: 04/15/2024
+
+# Description: Runs  folder_organiser or cruft_remover based on the user’s selection.
+
+$ Usage Information: Public
+
+PS3="Which script would you like to run?: "
+
+select script in "Cruft Remover" "Folder Organiser"; do
+
+	case "$script" in
+
+		"Cruft Remover") echo "Loading script.."
+
+				sleep 2
+
+				./cruft_remover.sh	
+
+				;;
+
+		"Folder Organiser") echo "Loading script.."
+
+				sleep 2
+
+				./folder_organiser.sh
+
+				;;
+
+		*) echo "Invalid Option";;
+
+	esac
+
+break
+
+done		
